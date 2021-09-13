d83ee32e1ee5ff24d81273f28a6ba1300e4edc6b
wesley guerra <wesleyguerra9@gmail.com>
2021-09-13 16:25:37 -0300

    finalizando aula 02 styled components

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container  = styled.View`
+    flex: 1;
+    justify-content: 'center';
+    align-content: 'center';
+    background-color: '#999';
+`;
+
+export const Title =  styled.Text`
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: purple;
+` 
